d9181133ed2b5fa760a9a58df9cb320de33cbc94
Aparna P Menon <menon.p.aparna@gmail.com>
2022-03-07 02:01:38 +0530

    Added Bootsrap and Authentication Service Component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -13,13 +14,14 @@ export class LoginComponent implements OnInit {
   errorMessage = 'invalid login'
   invalidLogin = false
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    public hardcodedauthentication: HardcodedAuthenticationService) { }
 
   ngOnInit(): void {
   }
 
   handleLogin(){
-    if(this.username==='aparna' && this.password==='dummy'){
+    if(this.hardcodedauthentication.authenticate(this.username, this.password)){
       this.router.navigate(['welcome', this.username])
     }else{
       this.invalidLogin=true
